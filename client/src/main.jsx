6a945fe67c6eee69ad4e266a9d9edfc8a4fa9df2
Avisek Sahoo <sahoo143avisek@gmail.com>
2026-06-10 03:08:07 +0530

    feat(client): add main entry point with BrowserRouter and providers

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.jsx"
+import { registerServiceWorker } from "./utils/sw-register"
+import { BrowserRouter } from "react-router-dom"
+import { AuthProvider } from "./context/AuthContext"
+
+registerServiceWorker()
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</BrowserRouter>
+	</StrictMode>,
+)
